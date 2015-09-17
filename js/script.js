6879ef95bd14a0d2ae1def71dc5b1877a82ee336
vinferno <vinsonfernandez@gmail.com>
2015-09-17 12:02:38 -0700

    update auto

---
+++ js/script.js
@@ -0,0 +1,24 @@
+alert("hi")
+var date = new Date().getDate();
+alert(date);
+
+var date2 = date -1;
+var today = date.toString();
+var yesterday = date2.toString();
+
+var as = document.getElementsByTagName('a');
+
+
+
+for(i=0;i<as.length;i++){
+	if ((i>5 && i< 12) || (i>17 && i<24)){
+	as[i].href=as[i].href + (date - 1);
+	alert(as[i].href);
+	}
+
+	if ((i>11 && i< 18) || (i>23 && i<30)){
+	as[i].href=as[i].href + (date);
+	alert(as[i].href);
+	}
+
+};
